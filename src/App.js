8cdc83ad1544be861edeefa9882eb850a8b5dbba
Asha Yalla <asha18.yalla@gmail.com>
2023-08-01 17:41:30 -0700

    reviewbuttons

--- src/App.js
+++ src/App.js
@@ -1,54 +1,20 @@
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-
-  )
-};
-
-const Content = (props) => {
-  return(
-    <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-
-    </div>
-  )
-
-}
-
-const Total = (props) => {
-	return (
-		<div>
-			<p>Number of exercises: {props.exercises1 + props.exercises2 + props.exercises3}</p>
-		</div>
-	)
-};
-
+import { useState } from 'react'
 
 
 const App = () => {
-  const course = 'Basics'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course = {course}/>
-      <Content part1 = {part1} part2 = {part2} part3 = {part3} exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
-      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
+      <button onClick={()=>{setGood(good+1)}}>Good</button> 
+      {good}
+      <button onClick={()=>{setNeutral(neutral+1)}}>Neutral</button> 
+      {neutral}
+      <button onClick={()=>{setBad(bad+1)}}>Bad</button> 
+      {bad}
     </div>
   )
 }
